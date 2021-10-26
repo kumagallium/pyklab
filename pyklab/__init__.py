4982bf7df7c0d42936dd6a68633383698a4b8bb4
kumagallium <kumagai.110h@gmail.com>
2021-10-26 11:57:42 +0900

    feat: add a visualization class

--- pyklab/__init__.py
+++ pyklab/__init__.py
@@ -1,4 +1,5 @@
 from .core.datasets import Datasets
 from .core.features import Features
 from .core.preprocess import Preprocess
+from .core.visualization import Visualization
 from .develop.ad import *
